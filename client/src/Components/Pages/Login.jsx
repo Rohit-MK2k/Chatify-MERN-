523c4ab93b2706b8a477e1297bae6ebfc4324681
Rohit-MK2K <rohitmalakar007@gmail.com>
2023-07-18 00:47:26 +0530

    form validation

--- client/src/Components/Pages/Login.jsx
+++ client/src/Components/Pages/Login.jsx
@@ -1,82 +1,68 @@
 import React,{useEffect, useState} from 'react'
 // import { validEmail, validPassword } from '../Shared/Regex'
 import { Link } from 'react-router-dom'
+import { useFormik } from 'formik'
+
+
+
 
 function Login() {
-    const form_des = 'flex flex-col justify-center items-center h-96 w-96 m-auto '
-    const login_des = 'border p-1 w-3/4 my-2'
-    const password_des = 'border p-1 w-3/4 my-2'
-    const submit_des = 'border-2 p-1 w-3/4 my-2 hover:cursor-pointer hover:bg-gray-400 hover:text-white disabled:bg-transparent disabled:text-black disabled:cursor-default'
+    const form_des = 'flex flex-col justify-center items-center h-96 reg-width m-auto '
+    const text_des = 'border p-1 w-3/4 '
+    const submit_des = 'border-2 p-1 w-3/4 my-2 hover:cursor-pointer hover:bg-gray-400 hover:text-white disabled:cursor-not-allowed disabled:bg-transparent disabled:text-black'
     const link_des = 'text-sm my-2 text-blue-600'
     const form_div = 'h-screen flex flex-col justify-center items-center'
-    
-    const [email,setEmail] = useState()
-    const [password,setPassword] = useState()
-    const [isDisable,setDisable] = useState(true)
-    const [eMessage,setEMessage] = useState("")
-    const [pMessage, setPMessage] = useState("")
-    useEffect(() => {
-        const debounce = setTimeout(() => {
-            validateEmail()
-        }, 4000)
-        return () => {
-            clearTimeout(debounce)
-        }
-    },[email])
-    useEffect(() => {
-        const debounce = setTimeout(() => {
-            validatePassword()
-        })
-        return () => {
-            clearTimeout(debounce)
-        }
-    },[password])
-    //Email validation
-    const emailPattern = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
-    const validateEmail = () =>{
-        if(email === ""){
-            setDisable(true);
-            setEMessage("*Please enter your email")
-            console.log(eMessage)
-        }
-        else if(!(emailPattern.test(email))){
-            setDisable(true);
-            setEMessage("*Invalid Email ID")
-            console.log(eMessage)
-        }
-        else{
-            setDisable(false)
-        }
-    }
-    //Password Validation
-    const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,10}$/
-    const validatePassword = (e) =>{
-        
-        if(password === ""){
-            setDisable(true)
-            setPMessage("*Please enter your password")
-            console.log(pMessage)
+    const error = 'text-sm text-red-600 flex flex-col justify-start p-1 w-3/4 h-5 mb-3'
 
-        }
-        else if(!(passwordPattern.test(password))){
-            setDisable(true)
-            setPMessage("*Invalid Password")
-            console.log(pMessage)
-        }
-        else{
-            setDisable(false)
-        }
+    const [disabled, setDisable] = useState(true)
+    
+    const validate = values => {
+    const errors = {}
+    const emailPattern = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+    const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,10}$/
+    if (!values.email) {
+        errors.email = '*Required'
     }
-    
-    
+    else if (!emailPattern.test(values.email)) {
+        errors.email = '*Invalid Email ID'
+    }
+    else {
+        setDisable(false)
+    }
+    if (!values.password) {
+        errors.password = '*Required'
+    }
+    else if (!passwordPattern.test(values.password)) {
+        errors.password = '*Invalid Password'
+    }
+    else {
+        setDisable(false)
+    }
+    return errors
+}
+
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: '',
+        },
+        validate,
+        onSubmit: values => {
+            console.log(JSON.stringify(values, null, 2))
+        },
+    })
+    console.log(formik.errors)
+
     return (
         <>
             <div className={form_div}>
-                <form action="" className={form_des}>
+                <form action="" className={form_des} onSubmit={formik.handleSubmit}>
                     <h2 className='text-4xl mb-6'>Log In</h2>
-                    <input type="email" onChange={(e)=>setEmail(e.target.value)} id="" placeholder='Email' value={email} className={login_des} />
-                    <input type="password" onChange={(e)=>setPassword(e.target.value)} placeholder='Password' value={password} className={password_des} />
-                    <input type="submit" value="Log In" disabled = {isDisable} className={submit_des} />
+                    <input type="email" onChange={formik.handleChange} onBlur={formik.handleBlur} id="email" name='email' placeholder='Email' value={formik.values.email} className={text_des} />
+                    {formik.touched.email && formik.errors.email ? <div className={error}>{formik.errors.email}</div>:<div className={error}></div>}
+                    <input type="password" onChange={formik.handleChange} onBlur={formik.handleBlur} placeholder='Password' id='password' name='password' value={formik.values.password} className={text_des} />
+                    {formik.touched.password && formik.errors.password ? <div className={error}>{formik.errors.password}</div>:<div className={error}></div>}
+                    <input type="submit" value="Log In" disabled = {disabled} className={submit_des} />
                     <Link to='/signup' className={link_des}>I don't have an account</Link>
                 </form>
             </div>
--- client/src/Components/Pages/Register.jsx
+++ client/src/Components/Pages/Register.jsx
@@ -1,97 +1,130 @@
 import React,{useState} from 'react'
 import { Link } from 'react-router-dom'
+import {useFormik } from 'formik'
+
+
+
 function Register() {
   const form_div = 'h-screen flex flex-col justify-center items-center'
   const form_des = 'flex flex-col justify-center items-center h-96 reg-width m-auto '
-  const text_des = 'border p-1 w-3/4 my-2'
-  const submit_des = 'border-2 p-1 w-3/4 my-2 hover:cursor-pointer hover:bg-gray-400 hover:text-white'
+  const text_des = 'border p-1 w-3/4 '
+  const submit_des = 'border-2 p-1 w-3/4 my-2 hover:cursor-pointer hover:bg-gray-400 hover:text-white disabled:cursor-not-allowed disabled:bg-transparent disabled:text-black'
   const link_des = 'text-sm my-2 text-blue-600'
-  const [email,setEmail] = useState()
-  const [password,setPassword] = useState()
-  const [fname,setFname] = useState()
-  const [lname,setLname] = useState()
-  const [conPassword,setConpassword] = useState()
+  const error = 'text-sm text-red-600 flex flex-col justify-start p-1 w-3/4 h-5 mb-3'
   const [isDisable, setDisable] = useState(true)
-  const [eMessage,setEMessage] = useState("")
-  const [pMessage,setPMessage] = useState("")
-  const [preview, setPreview] = useState('https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg')
-  const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,10}$/
-  const emailPattern = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
-  const handleEmail = (e) =>{
-    setEmail(e.target.value)
-    if(email === ""){
-      setDisable(true);
-      setEMessage("*Invalid Email ID")
-      console.log(eMessage)
+  const [preview, setPreview] = useState()
+  
+  
+  const validate = values => {
+    const emailPattern = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+    const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,10}$/
+    const errors = {}
+    if (!values.firstName) {
+      errors.firstName = '*Required'
     }
-    else if(!(emailPattern.test(email))){
-      setDisable(true);
-      setEMessage("*Invalid Email ID")
-      console.log(eMessage)
-    }
-    else{
+    else {
       setDisable(false)
     }
-  }
-  const handlePassword = (e) =>{
-    setPassword(e.target.value)
-    if(password === ""){
-      setDisable(true)
-      setPMessage("*Invalid Password")
-      console.log(pMessage)
+    if (!values.lastName) {
+      errors.lastName = '*Required'
     }
-    else if(!(passwordPattern.test(password))){
-      setDisable(true)
-      setPMessage("*Invalid Password")
-      console.log(pMessage)
+    else {
+      setDisable(false)
     }
-    else{
-      setDisable(true)
+    if (!values.email) {
+      errors.email = '*Required'
     }
-  }
-  const handleFname = (e) =>{
-    setFname(e.target.value)
-  }
-  const handleLname = (e) =>{
-    setLname(e.target.value)
-  }
-  const handleConPassword = (e) =>{
-    setConpassword(e.target.value)
-  }
-  const handleProfilePic = (e) => {
-    const file = e.target.files[0]
-    if (file) {
-      const reader = new FileReader()
-      reader.readAsDataURL(file)
-      reader.onloadend = () => {
-        setPreview(reader.result)
-      }
+    else if (!emailPattern.test(values.email)) {
+      errors.email = '*Invalid Email ID'
     }
-  }
+    else {
+      setDisable(false)
+    }
+    if (!values.password) {
+      errors.password = '*Required'
+    }
+    else if (!passwordPattern.test(values.password)) {
+      errors.password = '*Invalid Password'
+    }
+    else {
+      setDisable(false)
+    }
+    if (!values.conPassword) {
+      errors.conPassword = '*Required'
+    }
+    else if (!passwordPattern.test(values.conPassword)) {
+      errors.conPassword = '*Invalid Password'
+    }
+    else if (values.password !== values.conPassword) {
+      errors.conPassword = '*Password not matching'
+    }
+    else {
+      setDisable(false)
+    }
+  return errors
+}
+  
+  
+  
+  const formik = useFormik({
+    initialValues: {
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: '',
+      conPassword: '',
+      profilePic: '',
+    },
+    validate,
+    onSubmit: values => {
+      console.log(JSON.stringify(values, null, 2))
+      console.log(values.profilePic)
+    },
+  })
+  
+
   return (
     <>
       <div className={form_div}>
-        <form action='' className={form_des}>
+        <form action='' className={form_des} onSubmit={formik.handleSubmit}>
           <h2 className="text-4xl mb-6">Register</h2>
           <div className="flex flex-row justify-between w-3/4">
-            <input type="text" onChange={handleFname} className="w-2/4 border p-1 width-name my-2"   value={fname} placeholder='first name'/>
-            <input type="text" onChange={handleLname} className="w-2/4 border p-1 width-name my-2"  value={lname} placeholder='last name'/>
+            <div className='w-2/4 flex flex-col width-name'>
+              <input type="text" onChange={formik.handleChange} onBlur={formik.handleBlur} className="w-full border p-1  " name='firstName' value={formik.values.firstName} placeholder='first name' />
+              {formik.touched.firstName && formik.errors.firstName ? <div className={error}>{formik.errors.firstName}</div>:<div className={error}></div>}
+            </div>
+            <div className='w-2/4 flex flex-col width-name'>
+              <input type="text" onChange={formik.handleChange} onBlur={formik.handleBlur} className="w-full border p-1" name='lastName' value={formik.values.lastName} placeholder='last name' />
+              {formik.touched.lastName && formik.errors.lastName ? <div className={error}>{formik.errors.lastName}</div>:<div className={error}></div>}
+            </div>
           </div>
-          <input type="email" onChange={handleEmail} placeholder='Email' value={email} className={text_des} />
-          <input type="password"  onChange={handlePassword} placeholder='Password' value={password} className={text_des}/>
-          <input type="password" onChange={handleConPassword} placeholder='Confirm Password' value={conPassword} className={text_des} />
+          <input type="email" onChange={formik.handleChange} onBlur={formik.handleBlur} placeholder='Email' name='email' value={formik.values.email} className={text_des} />
+          {formik.touched.email && formik.errors.email ? <div className={error}>{formik.errors.email}</div>:<div className={error}></div>}
+          <input type="password" onChange={formik.handleChange} onBlur={formik.handleBlur} placeholder='Password' name='password' value={formik.values.password} className={text_des} />
+          {formik.touched.password && formik.errors.password ? <div className={error}>{formik.errors.password}</div>:<div className={error}></div>}
+          <input type="password" onChange={formik.handleChange} onBlur={formik.handleBlur} placeholder='Confirm Password' name='conPassword' value={formik.values.conPassword} className={text_des} />
+          {formik.touched.conPassword && formik.errors.conPassword ? <div className={error}>{formik.errors.conPassword}</div>:<div className={error}></div>}
           <div className='flex flex-col justify-center items-center'>
             <div className="h-40 w-40 rounded-full my-2 flex justify-center item-center">
               <img src={preview} alt="" className='rounded-full object-cover' />
             </div>
             <label htmlFor="profile" className='border-2 p-1.5 my-2 hover:cursor-pointer hover:bg-gray-400 hover:text-white'>
-              <input type="file" name="profile" id="profile" accept='image/*' className='hidden' onChange={(e) => handleProfilePic(e)} />
+              <input type="file" name="profilePic" id="profile" accept='image/*' className='hidden' onChange={(e) => {
+                const files = e.target.files[0]
+                if (files) {
+                  const reader = new FileReader()
+                  reader.readAsDataURL(files)
+                  reader.onloadend = () => {
+                    setPreview(reader.result)
+                    formik.setFieldValue('profilePic', reader)
+                  }
+                }
+              }} />
               Upload you Profile Picture
             </label>
           </div>
           <input type="submit" value="Submit" disabled = {isDisable} className={submit_des} />
           <Link to='/login' className={link_des}>Already have a Account</Link>
-          
         </form>
 
       </div>
